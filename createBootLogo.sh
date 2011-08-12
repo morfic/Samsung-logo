42369dd1b08a14169c2468700be0620fa5c03402
nubecoder <nubecoder@gmail.com>
2011-08-12 03:21:14 -0400

    Added createBootLogo.sh script for ease of use.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

---
+++ createBootLogo.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# createBootLogo.sh
+#
+# for use with morfic's Samsung-logo binary
+# https://github.com/morfic/Samsung-logo/
+#
+# 2011 nubecoder
+# http://www.nubecoder.com/
+#
+#
+# g++ -o makelogo makelogo.cpp > /dev/null 2>&1
+# ./makelogo > boot_logo.h
+# cat boot_logo.h > logo_rgb24_wvga_portrait_custom.h
+# cat charge_logo.h >> logo_rgb24_wvga_portrait_custom.h
+
+#defines
+DST_FILE="logo_rgb24_wvga_portrait_custom.h"
+BINARY="makelogo"
+BOOT_LOGO="boot_logo.h"
+CHARGE_LOGO="charge_logo.h"
+
+#functions
+SPACER()
+{
+	echo "*"
+}
+SHOW_COMPLETED()
+{
+	SPACER
+	echo "Script completed."
+	TIME_END=$(date +%s)
+	echo "Total time: $(($TIME_END - $TIME_START)) seconds."
+}
+BUILD_BINARY()
+{
+	echo "Building binary..."
+	# remove old files
+	rm -f $BINARY
+	# build binary
+	local RESULT=$(g++ -o $BINARY $BINARY.cpp 2>&1 >/dev/null)
+	# make binary executable
+	chmod +x $BINARY
+}
+CREATE_LOGO()
+{
+	echo "Creating bootlogo..."
+	# remove old files
+	rm -f $BOOT_LOGO
+	rm -f $DST_FILE
+	# convert header to usable data
+	./$BINARY > $BOOT_LOGO
+	# output to file
+	cat $BOOT_LOGO > $DST_FILE
+	cat $CHARGE_LOGO >> $DST_FILE
+}
+CLEANUP()
+{
+	echo "Cleaning up files..."
+	# remove files
+	rm -f $BOOT_LOGO
+	rm -f $BINARY
+}
+
+#main
+TIME_START=$(date +%s)
+SPACER
+BUILD_BINARY
+CREATE_LOGO
+CLEANUP
+SHOW_COMPLETED
+SPACER
+exit
+
